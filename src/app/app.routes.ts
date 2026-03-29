cd201137746a551739e5d807072503cb1fc1880b
Daniel-Brevess <danielbreves1.20@gmail.com>
2026-03-28 22:33:51 -0300

    Inicializando Plot!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then(m => m.HomePage),
+  },
+  {
+    path: 'feed',
+    loadComponent: () => import('./pages/feed/feed.page').then(m => m.FeedPage),
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./pages/profile/profile.page').then(m => m.ProfilePage),
+  },
+  {
+    path: 'edit-profile',
+    loadComponent: () => import('./pages/edit-profile/edit-profile.page').then(m => m.EditProfilePage),
   },
 ];
