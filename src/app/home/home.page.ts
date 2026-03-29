99b07ad8bfc511e65b28e859f15cc840bc24dcdd
Daniel-Brevess <danielbreves1.20@gmail.com>
2026-03-29 01:56:28 -0300

    Tirei o tailwind, o framework estava dando muito trabalho para configurar, então retirei ele e ja coloquei a minha parte do front-end (tela inicial, tela de perfil), fiz também os cards de cadastro e login)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonModal, IonItem, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterModule, IonModal, IonItem, IonInput],
 })
 export class HomePage {
   constructor() {}
