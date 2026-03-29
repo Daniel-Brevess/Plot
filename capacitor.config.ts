1b0b6f243cc3b5a614dd30dfda9467d941c5e513
Daniel-Brevess <danielbreves1.20@gmail.com>
2026-03-28 21:18:11 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'plot-app',
+  webDir: 'www'
+};
+
+export default config;
